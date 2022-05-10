52c3dbd481cc7369685041734aa612b374fc5de3
mayki-douglas <mayki.douglas1@gmail.com>
2022-05-10 04:37:05 -0300

    App com tela de login

---
+++ 06-layout/app/src/main/java/br/edu/qi/layoutnoite/LoginActivity.java
@@ -0,0 +1,15 @@
+package br.edu.qi.layoutnoite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
+}
